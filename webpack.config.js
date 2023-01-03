277485a34966d262a62e69f55fb82bd695ce929a
Tobi Oyero <tobioyero@gmail.com>
2023-01-02 21:27:53 -0500

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,38 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+import { resolve } from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin';
 
 
-module.exports = {
-    mode: 'development',
-    optimization: {
-        minimizer: [new OptimizeCSSAssetsPlugin({})],
+export const mode = 'development';
+export const optimization = {
+    minimizer: [new OptimizeCSSAssetsPlugin({})],
+};
+export const entry = resolve(__dirname, 'src/index.js');
+export const output = {
+    path: resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    clean: true,
+    assetModuleFilename: '[name][ext]'
+};
+export const plugins = [
+    new HtmlWebpackPlugin({
+        title: 'To-Do List',
+        filename: 'index.html',
+        template: 'src/template.html'
+    })
+];
+export const devtool = 'source-map';
+export const devServer = {
+    static: {
+        directory: resolve(__dirname, 'dist')
     },
-    entry: path.resolve(__dirname, 'src/index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
-        clean: true,
-        assetModuleFilename: '[name][ext]'
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'To-Do List',
-            filename: 'index.html',
-            template: 'src/template.html'
-        })
-    ],
-
-    devtool: 'source-map',
-
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, 'dist')
-        },
-        port: 3000,
-        open: true,
-        hot: true,
-        compress: true,
-        historyApiFallback: true
-    },
-
-   module: { 
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true
+};
+export const module = {
     rules: [
         {
             test: /\.scss$/i,
@@ -62,18 +57,16 @@ module.exports = {
         {
             test: /\.(woff|woff2|eot|ttf|otf)$/i,
             type: 'asset/resource',
-          },
+        },
 
         {
             test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
             use: {
-              loader: 'file-loader',
-              options: {
-                name: '[path][name].[ext]',
-              },
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]',
+                },
             },
         },
-        
     ]
-},
-}
+};
